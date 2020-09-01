3cae38e850fd65cb03756c7ed3d798949fa909cc
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-01 11:36:47 -0300

    init project

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderStyled=styled.div`
+
+`
+
+const Header = () => {
+    return (
+        <HeaderStyled>
+            
+        </HeaderStyled>
+    )
+}
+
+export default Header
