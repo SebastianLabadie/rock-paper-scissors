857ed21456c17e77cbf8aad5d4587604fdee0060
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-09-01 22:50:51 -0300

    Updates

---
+++ precache-manifest.32ca2fa7d572ffbe6402a258234ebb73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5585c8b925996fbe1f48f69d127647f8",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "a1932642ef24cf9114b1",
+    "url": "/rock-paper-scissors/static/css/main.f07caa9e.chunk.css"
+  },
+  {
+    "revision": "29cf3a0a1e7597473dbd",
+    "url": "/rock-paper-scissors/static/js/2.09bb2a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rock-paper-scissors/static/js/2.09bb2a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1932642ef24cf9114b1",
+    "url": "/rock-paper-scissors/static/js/main.c8ae2070.chunk.js"
+  },
+  {
+    "revision": "37a6b464d130ab040043",
+    "url": "/rock-paper-scissors/static/js/runtime-main.0d585ead.js"
+  }
+]);
